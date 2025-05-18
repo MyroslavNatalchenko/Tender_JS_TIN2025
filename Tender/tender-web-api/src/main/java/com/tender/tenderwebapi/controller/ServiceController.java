d8d0825fe01b222798b1488252cec374d4e13015
Myroslav Natalchenko <mirya@MacBook-Pro-Myroslav.local>
2025-05-18 14:02:21 +0200

    err

--- Tender/tender-web-api/src/main/java/com/tender/tenderwebapi/controller/ServiceController.java
+++ Tender/tender-web-api/src/main/java/com/tender/tenderwebapi/controller/ServiceController.java
@@ -4,13 +4,14 @@ import com.tender.tenderwebapi.services.TenderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-
+@CrossOrigin(origins = "*")
 public class ServiceController {
     private TenderService service;
 
